218c9e4948fb9a07ce560000d826f93abc3e0e79
adumbidiot <nathaniel.daniel23@outlook.com>
2020-10-26 20:19:42 -0700

    Add more info to ip_adapter_info

--- src/ip_adapter_info.rs
+++ src/ip_adapter_info.rs
@@ -1,12 +1,72 @@
 use crate::ip_addr_string::IpAddrString;
 use std::{
-    convert::TryInto,
+    convert::{
+        TryFrom,
+        TryInto,
+    },
     ffi::CStr,
 };
-use winapi::um::iptypes::{
-    IP_ADAPTER_INFO,
-    IP_ADDR_STRING,
+use winapi::{
+    shared::{
+        ipifcons::{
+            IF_TYPE_IEEE80211,
+            IF_TYPE_ISO88025_TOKENRING,
+            MIB_IF_TYPE_ETHERNET,
+            MIB_IF_TYPE_LOOPBACK,
+            MIB_IF_TYPE_OTHER,
+            MIB_IF_TYPE_PPP,
+            MIB_IF_TYPE_SLIP,
+        },
+        minwindef::DWORD,
+        ntdef::TRUE,
+    },
+    um::iptypes::{
+        IP_ADAPTER_INFO,
+        IP_ADDR_STRING,
+    },
 };
+
+/// The kind of adapter
+#[derive(Debug)]
+pub enum AdaperKind {
+    /// Some other type of network interface.
+    Other,
+
+    /// An Ethernet network interface.
+    Ethernet,
+
+    /// MIB_IF_TYPE_TOKENRING
+    TokenRing,
+
+    /// A PPP network interface.
+    PPP,
+
+    /// A software loopback network interface.
+    Loopback,
+
+    /// An ATM network interface.
+    ATM,
+
+    /// An IEEE 802.11 wireless network interface.
+    IEEE80211,
+}
+
+impl TryFrom<DWORD> for AdaperKind {
+    type Error = DWORD;
+
+    fn try_from(n: DWORD) -> Result<Self, Self::Error> {
+        match n {
+            MIB_IF_TYPE_OTHER => Ok(AdaperKind::Other),
+            MIB_IF_TYPE_ETHERNET => Ok(AdaperKind::Ethernet),
+            IF_TYPE_ISO88025_TOKENRING => Ok(AdaperKind::TokenRing),
+            MIB_IF_TYPE_PPP => Ok(AdaperKind::PPP),
+            MIB_IF_TYPE_LOOPBACK => Ok(AdaperKind::Loopback),
+            MIB_IF_TYPE_SLIP => Ok(AdaperKind::ATM),
+            IF_TYPE_IEEE80211 => Ok(AdaperKind::IEEE80211),
+            _ => Err(n),
+        }
+    }
+}
 
 /// Data about a network adapter
 #[repr(transparent)]
@@ -23,7 +83,7 @@ impl IpAdapterInfo {
         Iter::new(Some(self))
     }
 
-    /// Get the combo index.
+    /// Get the combo index. This is reserved.
     pub fn get_combo_index(&self) -> u32 {
         self.0.ComboIndex
     }
@@ -43,6 +103,21 @@ impl IpAdapterInfo {
         &self.0.Address[..self.0.AddressLength.try_into().unwrap()]
     }
 
+    /// The index. This is not persistent.
+    pub fn get_index(&self) -> u32 {
+        self.0.Index
+    }
+
+    /// Get the adapter kind.
+    pub fn get_kind(&self) -> Result<AdaperKind, u32> {
+        self.0.Type.try_into()
+    }
+
+    /// Check if dhcp is enabled for this adapter
+    pub fn dhcp_enabled(&self) -> bool {
+        self.0.DhcpEnabled == TRUE.into()
+    }
+
     /// A linked list of ip addresses associated with this adapter
     pub fn get_ip_address_list(&self) -> &IpAddrString {
         unsafe { &*(&self.0.IpAddressList as *const IP_ADDR_STRING as *const IpAddrString) }
@@ -57,9 +132,13 @@ impl IpAdapterInfo {
 impl std::fmt::Debug for IpAdapterInfo {
     fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
         fmt.debug_struct("IpAdapterInfo")
+            .field("combo_index", &self.get_combo_index())
             .field("name", &self.get_name().to_string_lossy())
             .field("description", &self.get_description().to_string_lossy())
             .field("address", &self.get_address())
+            .field("index", &self.get_index())
+            .field("kind", &self.get_kind())
+            .field("dhcp_enabled", &self.dhcp_enabled())
             .field("ip_address_list", &self.get_ip_address_list())
             .field("gateway_list", &self.get_gateway_list())
             .finish()
